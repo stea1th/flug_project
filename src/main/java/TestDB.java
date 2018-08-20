7b4de4387510113287b32817f13261857ceab68c
stea1th <stea1th@mail.ru>
2018-08-20 10:48:05 +0200

    Save Anrede and Passagier

--- src/main/java/TestDB.java
+++ src/main/java/TestDB.java
@@ -20,17 +20,13 @@ public class TestDB {
 
         List<String[]> xlsList = XLSReader.readXLS();
         xlsList.remove(0);
+        saveAnredeUndPassagier(xlsList);
+    }
 
+    public static void saveAnredeUndPassagier(List<String[]> xlsList){
         try(ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml", "spring/spring-db.xml")) {
             AnredeRestController anredeRestController = appCtx.getBean(AnredeRestController.class);
             PassagierRestController passagierRestController = appCtx.getBean(PassagierRestController.class);
-//            List<Anrede> list = anredeRestController.getAll();
-//            list.forEach(System.out::println);
-//            for(Anrede a : list){
-//                a.getPassagiers().forEach(System.out::println);
-//            }
-//            Anrede an = anredeRestController.create(new Anrede(5,""));
-//            passagierRestController.create(new Passagier(35,"Pavlik Durov"), an.getId());
 
             Map<String, Integer> anredeMap = anredeRestController.getAll()
                     .stream()
@@ -38,31 +34,21 @@ public class TestDB {
             List<Integer> passIds = passagierRestController.getAllIds();
 
             for(String[] arr : xlsList){
-                Passagier passagier = null;
-                if(!passIds.contains(Integer.parseInt(arr[0]))){
-                    passagier = new Passagier(Integer.parseInt(arr[0]), arr[2]);
-                    passIds.add(Integer.parseInt(arr[0]));
-                }
+                Integer id;
+                Integer pId = Integer.parseInt(arr[0]);
                 if(anredeMap.containsKey(arr[1])){
-                    if(passagier != null)
-                        passagierRestController.create(passagier, anredeMap.get(arr[1]));
+                    id = anredeMap.get(arr[1]);
                 }else{
                     Anrede an = new Anrede(CountUtil.getNewId(), arr[1]);
                     anredeMap.put(an.getBezeichnung(), an.getId());
                     anredeRestController.create(an);
-                    if(passagier != null)
-                        passagierRestController.create(passagier, an.getId());
+                    id = an.getId();
                 }
-            }
-
-
-            /*for(String[] arr : xlsList){
-                if(!anredeList.contains(arr[1])) {
-                    anredeRestController.create(new Anrede(arr[1]));
-                    anredeList.add(arr[1]);
+                if(!passIds.contains(pId)){
+                    passIds.add(pId);
+                    passagierRestController.create(new Passagier(pId, arr[2]), id);
                 }
-                passagierRestController.create(new Passagier(Integer.parseInt(arr[0]), arr[2]), arr[1]);
-                //System.out.println(arr[0]+" "+ arr[1]+" "+arr[2]);*/
             }
         }
+    }
 }
